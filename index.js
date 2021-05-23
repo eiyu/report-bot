1ae585f4a13fee0dc2011c974fe42f71c168cfca
yuliana <ysiulil22@gmail.com>
2021-05-23 10:23:46 +0700

    fix image url

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const CLIENT_KEY = 'ODQwMTE5MzIxOTAzODkwNDUy.YJTkFw.WNxGmNXxZly407dN3SjrzAVOktg';
-const DEV_KEY = 'ODQyOTM2MzgzNzU1NTE3OTY0.YJ8jrg.5qacWCrJpJXHMAYGlbTe_kqzF3E'
-const { Client, Intents, MessageEmbed, ClientUser, Message } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+require('dotenv').config()
+const { Client, Intents, MessageEmbed} = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 const {attachIsImage, ban} = require('./lib.js')
 const {store} = require('./db.js')
 
-const channelID = '840477001545941022' //'724648797300457532' //
+const channelID = process.env.channelID
 
+// need database for storing this id's 
 const banuser = {
   "362965490513215498": true, //me
   "696000898568028170": true,
   "269700566169550850": true,
   "706814151028113438": true,
   "692678251193303040": true,
-  "317283391982534666": true
+  "317283391982534666": true,
+  "845337870985068614": true
   }
 
-  // later use?
+  // later use for confirming id === name
+  // returning user
 const getUserFromMention = (mention) => {
   
-	const matches = mention.match(/^<@!?(\d+)>$/);
+	const matches = mention.match(/^<@!?(\d+)>$/)
 
-	if (!matches) return;
+	if (!matches) return
 
-	const id = matches[1];
+	const id = matches[1]
 
-	return client.users.cache.get(id);
+	return client.users.cache.get(id)
 }
 
 client.on('ready', () => {
-// channel.send(message)
-
-
-
+  
 console.log('ok..')
   client.user.setPresence({
       status: "online",  
@@ -40,77 +39,75 @@ console.log('ok..')
           name: "users all over the place",  
           type: "WATCHING" 
       }
-  });
-});
+  })
+})
 
 client.on('message', msg => {
   const [first, second, third, ...rest] = msg.content.split(' ')
-  if (!msg.guild) return;
+  if (!msg.guild) return
   // -------------------------------------------------------------- channel id
   const channel = client.channels.cache.find(channel => channel.id === channelID)
-// ||||||||||||||||||||||||||| capture data |||||||||||||||||||||||||||||||||||||||
-
-  // if any attachment
+  // error prone --------------------------
   if (msg.attachments.size > 0) {
     if(!msg.content || msg.content.length != 18) {
       return
     }
-  // if attachment images -------------------------------- replace with guild id
+    // if any attachment
+    // if attachment images -------------------------------- 
     if (msg.attachments.every(attachIsImage) && client.guilds.id !== 'guild_id'){
-      msg.client.users.fetch(msg.author.id).then(user => {
-        store.dispatch({type:'addImageURL', avatarURL: user.avatarURL()})
-        const data = {
-          messageId: msg.id,
-          name: msg.author.username,
-          guildId: msg.guild.id,
-          guildName: msg.guild.name,
-          userId: msg.author.id,
-          serverId: msg.guild.id,
-          userImage: user.avatarURL(),
-          channelId: msg.channel.id,
-          text: msg.content || 'No text image',
-          reviewed: false,
-          attachments: msg.attachments.map(attachment => {
-            return attachment.url
-          })[0]
-        }
-        
+      // GET SCAMMER PIC
+      msg.client.users.fetch(msg.content).then( scammer => {
+        msg.client.users.fetch(msg.author.id).then(user => {
+          // store.dispatch({type:'addImageURL', avatarURL: user.avatarURL()})
+          console.log('me', user.avatarURL())
+          const data = {
+            messageId: msg.id,
+            name: msg.author.username,
+            guildId: msg.guild.id,
+            guildName: msg.guild.name,
+            userId: msg.author.id,
+            serverId: msg.guild.id,
+            userImage: scammer.avatarURL(),
+            channelId: msg.channel.id,
+            text: msg.content || 'No text image',
+            reviewed: false,
+            attachments: msg.attachments.map(attachment => {
+              return attachment.url
+            })[0]
+          }
 
-        store.dispatch({type:'addFile', value: data})
-        
-        
-        const exampleEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        // get sender user profile image
-        .setTitle(data.userId)
-        .setThumbnail(data.userImage)
-        .addFields(
-          // get sender name
-          { name: 'Guild Name', value: data.guildName },
-          { name: 'Guild ID', value: data.guildId },
-          { name: 'Message ID', value: data.messageId },
-          { name: 'Reporter Name', value: data.name },
-          { name: '\u200B', value: '\u200B' },
-          // get sender text
-          { name: 'Scammer Id', value: data.text },
-          { name: 'Guide', value: `React to ✅ to ban the scammer or using .ban command \n React to ❌ to delete this embed`, inline: true },
-        )
-        .setImage(data.attachments)
-        .setTimestamp()
-        .setFooter('Report bot by: Sagara', 'https://github.com/eiyu');
+          store.dispatch({type:'addFile', value: data})
+          
+          const exampleEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(data.userId)
+          .setThumbnail(data.userImage)
+          .addFields(
+            { name: 'Guild Name', value: data.guildName },
+            { name: 'Guild ID', value: data.guildId },
+            { name: 'Message ID', value: data.messageId },
+            { name: 'Reporter Name', value: data.name },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Scammer Id', value: data.text },
+            { name: 'Guide', value: `React to ✅ to ban the scammer or using .ban command \n React to ❌ to delete this embed`, inline: true },
+          )
+          .setImage(data.attachments)
+          .setTimestamp()
+          .setFooter('Report bot by: Sagara', 'https://github.com/eiyu')
+  
+          channel.send(exampleEmbed).then(async embedMessage => {
+            await embedMessage.react('✅')
+            await embedMessage.react('❌')
+          }).catch(err => {
+            console.log(err)
+          })
+          channel.send(`Scammer id: ${data.text} \n Channel id: ${data.channelId}`)
+  
+          return
+        })
+      }).catch(err => {console.log(err)})
 
-        channel.send(exampleEmbed).then(async embedMessage => {
-          await embedMessage.react('✅');
-          await embedMessage.react('❌');
-        }).catch(err => {
-          console.log(err)
-        });
-        channel.send(`Scammer id: ${data.text} \n Channel id: ${data.channelId}`)
-
-        return;
-      })
-
-      return;
+      return
     }
   } 
 
@@ -126,8 +123,8 @@ client.on('message', msg => {
       .ban <userid> <guildid> - ban user id from server \n
       .wipe <userid> - ban user id (all server)
     `)
-    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu');
-    msg.channel.send(helpEmbed);
+    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu')
+    msg.channel.send(helpEmbed)
 
   }
   
@@ -138,24 +135,24 @@ client.on('message', msg => {
       return !st[v].reviewed
     })
     
-    const list = data.map(v => `- ${v} \n`);
+    const list = data.map(v => `- ${v} \n`)
     const helpEmbed = new MessageEmbed()
     .setColor('#0099ff')
     .setTitle('List of Unreviewed')
     .setDescription(list.length == 0 ? 'No unreviewed image' : list)
-    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu');
-    msg.channel.send(helpEmbed);
-    return;
+    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu')
+    msg.channel.send(helpEmbed)
+    return
   }
 
   if(msg.content.startsWith('.view') && !second) {
     if(msg.channel.id != channelID) {
       msg.channel.send(`Sorry, you don't have permission to this command`)
-      return;
+      return
     } 
 
     msg.channel.send(`Please type the Message ID`)
-    return;
+    return
   }
   if(msg.content.startsWith('.view') && second) {
     if(msg.channel.id != channelID) {
@@ -191,14 +188,14 @@ client.on('message', msg => {
     )
     .setImage(data.attachments)
     .setTimestamp()
-    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu');
+    .setFooter('Report bot by: Sagara', 'https://github.com/eiyu')
 
       msg.channel.send(exampleEmbed).then(async embedMessage => {
-        await embedMessage.react('✅');
-        await embedMessage.react('❌');
+        await embedMessage.react('✅')
+        await embedMessage.react('❌')
       }).catch(err => {
         console.log(err)
-      });
+      })
 
       msg.channel.send(`User id: ${data.userId} \n Channel id: ${data.channelId}`)
   }
@@ -236,9 +233,7 @@ client.on('message', msg => {
 
   if(msg.content.startsWith('.ban')) {
     // user who can ban
-    // console.log(banuser.hasOwnProperty(msg.client.user.id), 'ban')
     if(msg.channel.id != channelID && !banuser.hasOwnProperty(msg.client.user.id)) {
-      // console.log('shit')
       msg.channel.send(`Sorry, you don't have permission to this command`)
       return 
     }
@@ -251,7 +246,7 @@ client.on('message', msg => {
         
           guild.members.ban(user)
           msg.channel.send(`User ${user.username} has been banned from ${guild.name}`)
-          return;
+          return
 
         }).catch(err => {
           msg.channel.send(`Oops.. make sure to type user-id and guild-id correctly`)
@@ -264,25 +259,21 @@ client.on('message', msg => {
     }
   }
   
-});
+})
 
 
 
 client.on('messageReactionAdd', (reaction, user) => {
-  if(user.id != '' && reaction.message.channel.id != '840477001545941022') {
-    return;
+  if(user.id != '' && reaction.message.channel.id != channelID) {
+    console.log('m3ep')
+    return
   }
   const id = reaction.message.embeds[0].fields[2].value
   // scammerId
   const scammerid = reaction.message.embeds[0].fields[5].value
   const guildId = reaction.message.embeds[0].fields[1].value
-  // console.log(client.user.bot)
   
-  // console.log(!banuser.hasOwnProperty(user.id))
   if(!user.bot && reaction.emoji.name === '✅' && banuser.hasOwnProperty(user.id) ) {
-      // ban / kick
-      // console.log('meep')
-      
       client.guilds.fetch(guildId).then(guild => {
         guild.client.users.fetch(scammerid).then(scm => {
           
@@ -297,17 +288,16 @@ client.on('messageReactionAdd', (reaction, user) => {
       })
       let next = store.getState().value
       // mutation
-      next[id]['reviewed'] = true;
+      next[id]['reviewed'] = true
       store.dispatch({type: 'reviewed', value: next})
     }
     
-
     // add more flag like id if want to be safe
   if(!user.bot && reaction.emoji.name === '❌' ) {
-    reaction.message.delete();
-    return;
+    reaction.message.delete()
+    return
   }
 
-});
+})
 
-client.login(CLIENT_KEY);
+client.login(process.env.CLIENT_KEY)
